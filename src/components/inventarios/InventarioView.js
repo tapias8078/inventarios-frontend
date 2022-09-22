11727c0764cfe7b898c85f18b34936402824ecf5
dannytapias <67811683+tapias8078@users.noreply.github.com>
2022-09-22 09:39:37 -0500

    creacion de nuevos archivos

--- src/components/inventarios/InventarioView.js
+++ src/components/inventarios/InventarioView.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getInventarios } from '../../services/InventarioService';
 
-const InventarioView = () => {
+export const InventarioView = () => {
+  
+  const [ inventarios, setInventarios ] = useState ([]); 
+
+  const listarInventarios = async () => {
+    try {
+      const {data}= await getInventarios();
+      console.log(data); 
+      setInventarios(data)
+    } catch (error) {
+      console. log(error); 
+    }
+  }
+
+  useEffect(()=>{
+    listarInventarios ();    
+  },[]);
+
+  const handleOpenModal = () => {
+    console.log('asasas');
+    setOpenModal(!openModal)
+  }
+
   return (
-    <div>InventarioView</div>
+    <div className='container-fluid'>
+      <div className='mt-2 mb-2 row row-cols-1 row-cols-md-5 g-4'>
+          {
+            inventarios.map((inventario) => {
+              return <InventarioCard key={inventario._id} inventario={inventario}/>
+            })
+          }
+      </div>
+          {
+            openModal ? <InventarioNew/>:(
+              <button className='btn btn-primary fab' onClick={handleOpenModel}>
+                <i className='fa-solid fa-plus'></i>
+              </button>
+            )
+            
+          }
+    </div>
   )
-}
+        }
 
-export  {InventarioView}
+// export  {InventarioView}
